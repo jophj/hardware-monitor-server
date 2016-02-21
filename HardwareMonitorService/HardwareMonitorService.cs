e2f466722eaf5dc3abe6abd6c5e6d8212a6c7bf8
JoP <jop.mezzana@gmail.com>
2016-02-21 11:24:19 +0100

    Firewall rule is created by the service

--- HardwareMonitorService/HardwareMonitorService.cs
+++ HardwareMonitorService/HardwareMonitorService.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Configuration.Install;
+using System.Diagnostics;
 using System.ServiceProcess;
 using HttpServer;
 using Nancy.Hosting.Self;
@@ -17,9 +17,32 @@ namespace HardwareMonitorService
 
         protected override void OnStart(string[] args)
         {
+            CreateFirewallRule(6620);
+
             string uri = "http://localhost:" + Bootstrapper.WebServerPort + "/";
             _host = new NancyHost(new Uri(uri));
             _host.Start();
+        }
+
+        private void CreateFirewallRule(int webServerPort)
+        {
+            Process deleteRule = new Process();
+            deleteRule.StartInfo.FileName = "netsh";
+            deleteRule.StartInfo.Arguments =
+                $"advfirewall firewall delete rule name=\"Allow hardware monitor\"";
+            deleteRule.StartInfo.CreateNoWindow = true;
+            deleteRule.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            deleteRule.Start();
+            deleteRule.WaitForExit();
+
+            Process addRule = new Process();
+            addRule.StartInfo.FileName = "netsh";
+            addRule.StartInfo.Arguments =
+                $"advfirewall firewall add rule name=\"Allow hardware monitor\" protocol=TCP dir=in localport={webServerPort} action=allow";
+            addRule.StartInfo.CreateNoWindow = true;
+            addRule.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            addRule.Start();
+            addRule.WaitForExit();
         }
 
         protected override void OnStop()
