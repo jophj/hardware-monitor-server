154ace51eabb50e662134df97de4c8a489a6599e
JoP <jop.mezzana@gmail.com>
2016-02-21 11:18:30 +0100

    Service is not starting

--- HardwareMonitorService/HardwareMonitorService.cs
+++ HardwareMonitorService/HardwareMonitorService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Configuration.Install;
+using System.Diagnostics;
+using System.IO;
 using System.ServiceProcess;
-using HttpServer;
 using Nancy.Hosting.Self;
 
 namespace HardwareMonitorService
@@ -9,17 +9,54 @@ namespace HardwareMonitorService
     partial class HardwareMonitorService : ServiceBase
     {
         private NancyHost _host;
+        private StreamWriter _logStream { get; set; }
 
         public HardwareMonitorService()
         {
+            _logStream = new StreamWriter("service.log");
+            _logStream.WriteLine("InitializeComponent()");
             InitializeComponent();
         }
 
+
         protected override void OnStart(string[] args)
         {
-            string uri = "http://localhost:" + Bootstrapper.WebServerPort + "/";
+            int webServerPort = 6620;
+            //SettingsManager settingsManager = new SettingsManager();
+            //int webServerPort = settingsManager.GetSettings().WebServerPort;
+
+            //CreateFirewallRule(webServerPort);
+            _logStream.WriteLine("Starting service...");
+            _logStream.Flush();
+
+            string uri = "http://localhost:" + webServerPort + "/";
             _host = new NancyHost(new Uri(uri));
+            _logStream.WriteLine("Starting Nancy...");
+            _logStream.Flush();
             _host.Start();
+            _logStream.WriteLine("service started...");
+            _logStream.Flush();
+        }
+
+        private void CreateFirewallRule(int webServerPort)
+        {
+            Process deleteRule = new Process();
+            deleteRule.StartInfo.FileName = "netsh";
+            deleteRule.StartInfo.Arguments =
+                $"advfirewall firewall delete rule name=\"Allow hardware monitor\"";
+            deleteRule.StartInfo.CreateNoWindow = true;
+            deleteRule.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            deleteRule.Start();
+            deleteRule.WaitForExit();
+
+            Process addRule = new Process();
+            addRule.StartInfo.FileName = "netsh";
+            addRule.StartInfo.Arguments =
+                $"advfirewall firewall add rule name=\"Allow hardware monitor\" protocol=TCP dir=in localport={webServerPort} action=allow";
+            addRule.StartInfo.CreateNoWindow = true;
+            addRule.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            addRule.Start();
+            addRule.WaitForExit();
         }
 
         protected override void OnStop()
--- HardwareMonitorService/Program.cs
+++ HardwareMonitorService/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.ServiceProcess;
+﻿using System.IO;
+using System.ServiceProcess;
+using Nancy.ViewEngines;
+using Newtonsoft.Json;
 
 namespace HardwareMonitorService
 {
@@ -9,12 +12,57 @@ namespace HardwareMonitorService
         /// </summary>
         static void Main()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            var _logStream = new StreamWriter("service.log");
+            _logStream.WriteLine("dsadasd");
+            _logStream.Flush();
+
+            var ServicesToRun = new ServiceBase[]
             {
                 new HardwareMonitorService(),
             };
             ServiceBase.Run(ServicesToRun);
         }
+    }
+
+    public class SettingsManager
+    {
+        private readonly string _settingsFileName = "settings.json";
+
+        private readonly ServiceSettings _defaultSettings = new ServiceSettings()
+        {
+            WebServerPort = 6620
+        };
+
+        public ServiceSettings GetSettings()
+        {
+            string fileContent = ReadSettingsFile();
+            if (string.IsNullOrEmpty(fileContent))
+            {
+                return _defaultSettings;
+            }
+
+            return JsonConvert.DeserializeObject<ServiceSettings>(fileContent);
+        }
+
+        private string ReadSettingsFile()
+        {
+            StreamReader fileStream = null;
+            try
+            {
+                fileStream = new StreamReader(_settingsFileName);
+            }
+            catch
+            {
+                return null;
+            }
+
+            string settingsContent = fileStream.ReadToEnd();
+            return settingsContent;
+        }
+    }
+
+    public class ServiceSettings
+    {
+        public int WebServerPort { get; set; }
     }
 }
