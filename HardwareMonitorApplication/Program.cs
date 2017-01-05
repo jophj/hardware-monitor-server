1b3797875f46876c06940584bd9669fe4adabf5e
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-01-05 11:29:03 +0100

    Application install service and doesn't run httpserver

--- HardwareMonitorApplication/Program.cs
+++ HardwareMonitorApplication/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
-using Nancy.Hosting.Self;
-using System.Threading;
+using HardwareMonitorService;
 
 namespace HardwareMonitorApplication
 {
@@ -12,14 +11,22 @@ namespace HardwareMonitorApplication
         {
             var hardwareMonitorServiceExe = new FileInfo("HardwareMonitorService.exe");
 
+            ServiceManager serviceManager = new ServiceManager("HardwareMonitor", hardwareMonitorServiceExe.FullName);
+            serviceManager.StopService();
+            serviceManager.DeleteService();
+            serviceManager.CreateService();
+            serviceManager.StartService();
+
+            EventLogger.LogDebug("Application started");
+
             // use this code to run service code inside application
-            string uri = $"http://localhost:{6620}/";
-            var _host = new NancyHost(new Uri(uri));
-            _host.Start();
-            while (true)
-            {
-                Thread.Sleep(128);
-            }
+            // string uri = $"http://localhost:{6620}/";
+            // var _host = new NancyHost(new Uri(uri));
+            // _host.Start();
+            // while (true)
+            // {
+            //      Thread.Sleep(128);
+            // }
         }
     }
 }
